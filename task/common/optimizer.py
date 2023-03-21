122cdcc2a03b99ff97e907843e180a1a2fc9a261
foracgn <729871233@qq.com>
2023-03-21 15:11:36 +0800

    fix:optimizer参数修正

--- task/common/optimizer.py
+++ task/common/optimizer.py
@@ -61,7 +61,7 @@ def generateOptimizer(cfgs, model):
             )
         )
         scheduler.append(
-            cfgs.optimizer_cfgs['optimizer_{}_scheduler'.format(i)](
+            cfgs.optimizerConfigs['optimizer_{}_scheduler'.format(i)](
                 optimizer[i],
                 **cfgs.optimizer_cfgs['optimizer_{}_scheduler_args'.format(i)]
             )
