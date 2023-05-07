1aa72a0dd8e5ab169ffdaf42a65f02b90eceddb8
foracgn <729871233@qq.com>
2023-05-07 22:26:59 +0800

    oracle task

---
+++ task/OCR/oracle/test.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+from task.model.DAN import base
+from task.framework import baseline
+import yaml
+
+constPath = "./task/yaml/oracle.yaml"
+
+if __name__ == '__main__':
+    data = open(constPath, 'r').read()
+    setConfigs = yaml.load(data, Loader=yaml.FullLoader)
+    modelRoot = setConfigs['modelRoot']
+    modelPath = []
+    for i in range(0, 4):
+        taskPath = [
+            modelRoot + setConfigs['basic']['FE' + str(i)],
+            modelRoot + setConfigs['basic']['CAM' + str(i)],
+            modelRoot + setConfigs['basic']['DTD'+str(i)],
+            modelRoot + setConfigs['basic']['PE'+str(i)]
+        ]
+        modelPath.append(taskPath)
+
+    trainDict = ""
+    testDict = setConfigs['oracle_dict']
+    testRoot = setConfigs['oracle_root']
+
+    cfgs = base.FreeDictDanConfig(
+            modelPath[0],
+            testDict,
+            [testRoot],
+            [testRoot],
+            mode="Test"
+    )
+
+    runner = baseline.BaselineDAN(cfgs)
+    runner.runTest(modelRoot, True)
+
+    print("oracle task done")
